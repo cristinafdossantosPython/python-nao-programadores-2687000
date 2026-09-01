8570fe5e0bda920a8284bba1b4ca3b445a8a8499
cristinafdossantosPython <cristinafdossantos@icloud.com>
2026-09-01 14:52:24 +0000

    Resolução exercicio_numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -9,3 +9,23 @@ altura = 1.74
 
 
 # Realize uma operação entre dados do tipo inteiro e float
+#Resolução do exercício
+data_nascimento = '15-12-1980'
+idade_numerica = 43
+altura = 1.58
+salario = 7500
+beneficios = 1250.55
+#variáveis numéricas criadas acima
+# Realize uma operação entre dados do tipo string e inteiro
+soma = salario + beneficios
+print(soma)
+print(type(soma))
+#Reinício
+data_nascimento = '15-12-1980'
+idade_numerica = 45
+#variáveis numéricas criadas acima
+# Descubra o tipo de dado de cada variável acima
+print(type(idade_numerica))
+idade = 45
+print(type(idade))
+#Resolvido abaixo, terminal
